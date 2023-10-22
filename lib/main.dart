6412cdbea5e4b10a87e4301164e6e2eefad94daf
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-10-22 15:22:36 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:whatappclone/NavaBar.dart';
 import 'package:whatappclone/screen/HomePage.dart';
 
 void main() {
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: const NavBar(),
       debugShowCheckedModeBanner: false,
     );
 
