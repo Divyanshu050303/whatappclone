03fe68ab39469a9569d7d3ecfeec01623975eff3
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-10-22 15:22:25 +0530

    separate the navbar form the home screen

--- lib/screen/HomePage.dart
+++ lib/screen/HomePage.dart
@@ -12,34 +12,6 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     MediaQueryData mediaQueryData=MediaQuery.of(context);
     // Color color=Color(#08846c);
-    return Container(
-      height: mediaQueryData.size.height,
-      width: mediaQueryData.size.width,
-      color: Colors.white,
-      child: Column(
-        children: [
-          Container(
-            width: mediaQueryData.size.width,
-            height: mediaQueryData.size.height*0.2,
-            color: Color.fromARGB(500, 5,130,107),
-            child: Padding(
-              padding:  EdgeInsets.only(top: mediaQueryData.size.width*0.12),
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      Text("WhatsApp", style: TextStyle(fontSize: 24, color: Colors.white, decoration: TextDecoration.none,   ),),
-                      Icon(Icons.camera_alt_outlined, color: Colors.white,size: 30,),
-                      Icon(Icons.search_outlined, color: Colors.white, size: 30,),
-                      CircleAvatar()
-                    ],
-                  )
-                ],
-              ),
-            ),
-          )
-        ],
-      ),
-    );
+    return Placeholder();
   }
 }
