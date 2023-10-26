0c8a56235e7201783758e8a161d2774f140d549a
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-10-26 22:10:39 +0530

    Paint the group screen

--- lib/screen/Groups.dart
+++ lib/screen/Groups.dart
@@ -11,13 +11,60 @@ class _GroupsState extends State<Groups> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-title: Container(
-  color: Colors.green,
-  width: 700,
-  height: 400,
-  child: Center(child: Text("Whatappssfaffdaahsjdfajshfdajsndfalshfiasefnasdlkjfhasdhfasndflasjhdfiaushdfnas fldaslvhaf dsajfiajsdfiuhsadf saf jasdoifansdfnasdfjoaisdfnasdmfnasdhfasdfsfjsldkfj;asdjfasndfsahdfo;asndfknasdl;kfaskjdnfasdkf;kajdfnksdnf;alkdfjkashfjdlhaiuefhkdjnajdksvahsfxcnasdjfnalsdkfnaslkjfhlaskjdfmnaflkj", style: TextStyle(color: Colors.red),)),
-),
+      body: Stack(
+        children: [
+          Positioned(
+            top: 30,
+            left: 18,
+            child: Row(
+              children: [
+                Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.circular(10)),
+                  child: const Icon(
+                    Icons.groups,
+                    color: Colors.white,
+                    size: 25,
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 13.0),
+                  child: Stack(
+                    children: [
+                      Text(
+                        "New community",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18),
+                      ),
+
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          Positioned(
+            left: 48,
+            top: 62,
+            child: Container(
+              height: 22,
+              width: 22,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: const Color.fromARGB(500, 5, 130, 107),
+              ),
+              child: const Icon(
+                Icons.add,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
